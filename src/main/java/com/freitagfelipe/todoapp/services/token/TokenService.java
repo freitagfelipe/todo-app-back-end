f01587e7e42f77c3770397fb3754e48ed3ea9129
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-12-08 11:36:48 -0300

    Create a token service

---
+++ src/main/java/com/freitagfelipe/todoapp/services/token/TokenService.java
@@ -0,0 +1,57 @@
+package com.freitagfelipe.todoapp.services.token;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.freitagfelipe.todoapp.domain.user.UserEntity;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Optional;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(UserEntity user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(this.secret);
+
+            return JWT.create()
+                    .withIssuer("todo-app-server")
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(this.generateExpirationDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Error while generating JWT token", exception);
+        }
+    }
+
+    public Optional<String> validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(this.secret);
+
+            return Optional.of(JWT.require(algorithm)
+                    .withIssuer("todo-app-server")
+                    .build()
+                    .verify(token)
+                    .getSubject());
+        } catch (JWTVerificationException exception) {
+            return Optional.empty();
+        }
+    }
+
+    private Instant generateExpirationDate() {
+        return LocalDateTime
+                .now()
+                .plusHours(2)
+                .toInstant(ZoneOffset.of("-03:00"));
+    }
+
+}
